abf8ac540b269e1f1d40d99dabd7e539cea34efc
kevin dwi septian <dskevin32@gmail.com>
2022-03-08 21:52:33 +0800

    firstcommit

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\produk;
+use Illuminate\Http\Request;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(produk $produk)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    // return "string";
+});
+
+Route::get('/test', function(){
+    return view('layout.template');
+});
+Route::get('/admin', function(){
+    return view('admin.index');
+});
